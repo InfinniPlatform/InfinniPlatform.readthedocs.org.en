88032692f25226a2bf4a6c40cbfb7f0f3e2f2703
AlexMAS <as.mezhov@gmail.com>
2017-02-09 16:53:16 +0500

    Add check_apiReference

---
+++ docs/check_apiReference.ps1
@@ -0,0 +1,30 @@
+$success = $true
+$apiFiles = Get-ChildItem -Path (Join-Path $PSScriptRoot 'source/_docfx/api/reference') -Filter '*.html' | ForEach-Object { $_.Name }
+$docFiles = Get-ChildItem -Path (Join-Path $PSScriptRoot 'source') -Filter '*.rst' -Recurse
+
+foreach ($docFile in $docFiles)
+{
+    $docContent = Get-Content $docFile.FullName
+
+    $references = $docContent | Select-String '(?mi)^.*?\:\s*\/api\/reference\/(.*)\.html.*?$' -AllMatches `
+        | ForEach-Object { $_.Matches.Groups[1] } `
+        | ForEach-Object { $_.Value + '.html' }
+
+    foreach ($ref in $references)
+    {
+        if ($apiFiles -notcontains $ref)
+        {
+            Write-Host "File '$($docFile.FullName)' contains bad reference '$ref'." -ForegroundColor Yellow
+            $success = $false
+        }
+    }
+}
+
+if (-not $success)
+{
+    Write-Host 'Documentation contains bad references to the API.' -ForegroundColor Red
+    exit 1
+}
+
+Write-Host 'All references to the API are actual.' -ForegroundColor Green
+exit 0
